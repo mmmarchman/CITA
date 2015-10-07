fd8dc0f64a8e846d3f96c121d3a26787b302ff41
McClain Marchman <mmmarchman@gmail.com>
2015-10-07 13:31:20 -0400

    working

--- Web/app/__init__.py
+++ Web/app/__init__.py
@@ -14,7 +14,7 @@ db = SQLAlchemy()
 pagedown = PageDown()
 
 login_manager = LoginManager()
-login_manager.session_protection = 'strong'
+# login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 
@@ -40,7 +40,5 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-    from .api_1_0 import api as api_1_0_blueprint
-    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
 
     return app
--- Web/app/models.py
+++ Web/app/models.py
@@ -2,11 +2,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin
 from . import db, login_manager
 
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
