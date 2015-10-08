520fa0f718e257ee5e75666f02be6366e12834d2
McClain Marchman <mmmarchman@gmail.com>
2015-10-07 22:03:37 -0400

    FinallY!!! WORKING

--- Web/manage.py
+++ Web/manage.py
@@ -70,10 +70,7 @@ def deploy():
     upgrade()
 
     # create user roles
-<<<<<<< HEAD
-=======
     # Role.insert_roles()
->>>>>>> 4b29ce634eac608f7139c55fe1dc2294deb81083
 
 
 if __name__ == '__main__':
