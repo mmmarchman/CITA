7a1ab66862ef08605678b4174dde2c9d00bc2e45
McClain Marchman <mmmarchman@gmail.com>
2015-10-18 16:11:20 -0400

    Updated requirements, added links for ntlk in NOTES.txt, started work on Parser.py

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,2 +1,18 @@
+#!/usr/bin/env python
+
+import nltk
 
 __author__ = 'McClain Marchman'
+
+
+class Parser(object):
+    list_strings = None
+    dict_top_words = None
+
+    def __init__(self, list_strings):
+        self.list_of_strings = list_strings
+
+
+    def parse(self):
+        stemmer
+
