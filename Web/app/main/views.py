9d61c1eea17b52c5c6ac565475b47c02b5c1ba94
Doug Oakes <doug.oakes@gmail.com>
2015-10-20 04:48:11 -0400

    Corrected an issue on logout
    Session variable for company id was not being released, causing crash on
    logout

--- Web/app/main/views.py
+++ Web/app/main/views.py
@@ -21,8 +21,16 @@ def dictionary_factory(cursor, row):
 @main.before_request
 def before_request():
     g.user = current_user.get_id()
-    g.companyid = current_user.company_id
+    if current_user.is_authenticated:
+        g.companyid = current_user.company_id
     g.citadb = get_connection()
+
+
+@main.route("/logout")
+@login_required
+def logout():
+    g.session.delete(companyid)
+    return render_template('index.html')
 
 
 @main.route('/')
