d61bc621c27176efd89ba5a29e07721e899a02bf
McClain Marchman <mmmarchman@gmail.com>
2015-10-20 13:29:11 -0400

    fixed creds

--- Web/app/main/Plotly.py
+++ Web/app/main/Plotly.py
@@ -6,9 +6,12 @@ __author__ = 'McClain Marchman'
 
 import plotly.plotly as py
 import plotly.graph_objs as go
-
+import plotly.tools as tls
 
 class TestPlotly:
+
+    tls.tools.set_credentials_file(username='mmmarchman', api_key='jqfkdptmgy')
+
 
     word_count_list = None
 
