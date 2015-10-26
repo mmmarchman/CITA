e28a3ca2a29cb8ee86494cee4164ad1662b0120d
McClain Marchman <mmmarchman@gmail.com>
2015-10-26 17:12:03 -0400

    error checking

--- Web/app/main/views.py
+++ Web/app/main/views.py
@@ -14,9 +14,12 @@ def get_connection():
     # Combines current working directory with the name of the db
     # so that we can operate on multiple platforms
     cwd = os.getcwd()
-    print cwd
+
+    output_file = open('outputFile.txt','rw+')
     db_location = os.path.join(cwd, 'data-dev.sqlite')
+    output_file.write(db_location)
     conn = sqlite3.connect(db_location)
+    output_file.close()
     return conn
 
 
