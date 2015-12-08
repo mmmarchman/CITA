d7fdb3a8b81431cd26a1f12d12a2f1eb05f0b853
McClain Marchman <mmmarchman@gmail.com>
2015-12-08 14:10:59 -0500

    BAD

--- Web/config.py
+++ Web/config.py
@@ -11,8 +11,8 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = 'citawebservice@gmail.com'
+    MAIL_PASSWORD = 'TECHDucZoo4I'
     CITA_MAIL_SUBJECT_PREFIX = '[CITA]'
     CITA_MAIL_SENDER = 'CITA Admin <citawebservice@gmail.com>'
     CITA_ADMIN = os.environ.get('CITA_ADMIN')
