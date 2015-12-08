e5a6ee995bcc9cabf79a22d49e52bb096c244aac
McClain Marchman <mmmarchman@gmail.com>
2015-12-08 12:48:22 -0500

    trying to fix email bug

--- Web/config.py
+++ Web/config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
