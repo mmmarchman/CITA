93d0984edf94d8dc8ade59d65e5a04352ce3e6b3
McClain Marchman <mmmarchman@gmail.com>
2015-12-08 13:32:04 -0500

    switching plotly account so we can present

--- Web/app/email.py
+++ Web/app/email.py
@@ -15,6 +15,7 @@ def send_email(to, subject, template, **kwargs):
                   sender=app.config['CITA_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
+    mail.send(msg)
+    # thr = Thread(target=send_async_email, args=[app, msg])
+    # thr.start()
+    # return thr
